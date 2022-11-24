030a5443609f5f82a6ee492ef5104144599c17c6
10Sadique <jafarsjahan@gmail.com>
2022-11-25 03:53:15 +0600

    log removed

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -8,7 +8,6 @@ const useUser = (email) => {
         fetch(`http://localhost:5000/users/${email}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
                 setIsUser(data.isUser);
                 setIsUsreLoading(false);
             });

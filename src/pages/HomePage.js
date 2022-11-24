b69ee8ff4d885a22db0c65aaf668e69a927dd25c
10Sadique <jafarsjahan@gmail.com>
2022-11-24 17:41:31 +0600

    react query and axios added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 const HomePage = () => {
+    const { data: categories, isLoading } = useQuery({
+        queryKey: ['categories'],
+        queryFn: async () => {
+            const res = await axios('http://localhost:5000/categories');
+
+            return res;
+        },
+    });
+
+    console.log(categories);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <div className="mx-auto max-w-[370px] md:max-w-3xl lg:max-w-6xl my-10 lg:my-14">

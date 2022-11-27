b25c4df8ce6c48c5eee506bae45b7eba486e5366
10Sadique <jafarsjahan@gmail.com>
2022-11-27 16:33:58 +0600

    2nd deploy to firebase

--- src/components/ui/Banner.js
+++ src/components/ui/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 my-24 gap-10">
             <div className="order-2 lg:order-1 flex flex-col items-start justify-center">
-                <h1 className="text-6xl font-bold mb-5 text-primary">
+                <h1 className="lg:text-6xl text-5xl font-bold mb-5 text-primary">
                     Get The Best Laptops at a Reasonable Price
                 </h1>
                 <p className="text-gray-500 mb-5">
